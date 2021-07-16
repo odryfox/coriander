7f305f1af760bbe335d9f293c5d705c6f24fd69e
Vyacheslav Rusov <odryfox@gmail.com>
2021-07-16 16:38:24 +0300

    Refactor matcher tests

--- tests/test_matcher.py
+++ tests/test_matcher.py
@@ -1,185 +1,229 @@
+from unittest import mock
+
 from coriander.matching import DefaultMatcher, Matcher
+from coriander.tokenizers import DefaultTokenizer
 from coriander.tokens import AnyToken, CharToken
 
 
-def test_correct_message():
-    tokens = [
-        CharToken(char="h"),
-        CharToken(char="e"),
-        AnyToken(),
-        CharToken(char="o"),
-    ]
-    match_tokens_with_message_results = Matcher(tokenizer=None).match_with_tokens(
-        tokens=tokens,
-        message="hello",
-    )
-    assert match_tokens_with_message_results[0].end == 5
+class TestMatcher:
+    def test_match_with_tokens(self):
+        tokens = [
+            CharToken(char="h"),
+            CharToken(char="e"),
+            AnyToken(),
+            CharToken(char="o"),
+        ]
+        tokenizer = mock.Mock()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        match_tokens_with_message_results = matcher.match_with_tokens(
+            tokens=tokens,
+            message="hello",
+        )
+
+        assert match_tokens_with_message_results[0].end == 5
+        assert match_tokens_with_message_results[0].context == {}
+
+    def test_match_with_tokens__incorrect(self):
+        tokens = [
+            CharToken(char="h"),
+            CharToken(char="e"),
+            AnyToken(),
+            CharToken(char="a"),
+        ]
+        tokenizer = mock.Mock()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        match_tokens_with_message_results = matcher.match_with_tokens(
+            tokens=tokens,
+            message="hello",
+        )
+
+        assert match_tokens_with_message_results == []
+
+    def test_match_with_tokens__empty_message(self):
+        tokens = [
+            CharToken(char="h"),
+            CharToken(char="e"),
+            AnyToken(),
+            CharToken(char="o"),
+        ]
+        tokenizer = mock.Mock()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        match_tokens_with_message_results = matcher.match_with_tokens(
+            tokens=tokens,
+            message="",
+        )
+
+        assert match_tokens_with_message_results == []
+
+    def test_match_with_tokens__empty_tokens_and_empty_message(self):
+        tokens = []
+        tokenizer = mock.Mock()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        match_tokens_with_message_results = matcher.match_with_tokens(
+            tokens=tokens,
+            message="",
+        )
+
+        assert match_tokens_with_message_results[0].end == 0
+        assert match_tokens_with_message_results[0].context == {}
+
+    def test_match_with_tokens__empty_tokens_and_message(self):
+        tokens = []
+        tokenizer = mock.Mock()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        match_tokens_with_message_results = matcher.match_with_tokens(
+            tokens=tokens,
+            message="hello",
+        )
+
+        assert match_tokens_with_message_results[0].end == 0
+        assert match_tokens_with_message_results[0].context == {}
+
+    def test_match_with_tokens__few_variants(self):
+        tokens = [
+            AnyToken(),
+            CharToken(char="e"),
+        ]
+        tokenizer = mock.Mock()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        match_tokens_with_message_results = matcher.match_with_tokens(
+            tokens=tokens,
+            message="hee",
+        )
+
+        assert match_tokens_with_message_results[0].end == 2
+        assert match_tokens_with_message_results[0].context == {}
+        assert match_tokens_with_message_results[1].end == 3
+        assert match_tokens_with_message_results[1].context == {}
+
+    def test_match_with_tokens__any_token(self):
+        tokens = [
+            AnyToken(),
+        ]
+        tokenizer = mock.Mock()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        match_tokens_with_message_results = matcher.match_with_tokens(
+            tokens=tokens,
+            message="hee",
+        )
+
+        assert match_tokens_with_message_results[0].end == 1
+        assert match_tokens_with_message_results[0].context == {}
+        assert match_tokens_with_message_results[1].end == 2
+        assert match_tokens_with_message_results[1].context == {}
+        assert match_tokens_with_message_results[2].end == 3
+        assert match_tokens_with_message_results[2].context == {}
+
+    def test_match__associate_name(self):
+        tokenizer = DefaultTokenizer()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        result = matcher.match(
+            message="hello",
+            template="[hello|hi]~greeting",
+        )
+
+        assert result
+        assert result.success
+        assert result.context["greeting"] == "hello"
+
+    def test_match__associate_name_second_variant(self):
+        tokenizer = DefaultTokenizer()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        result = matcher.match(
+            message="hi",
+            template="[hello|hi]~greeting",
+        )
+
+        assert result
+        assert result.success
+        assert result.context["greeting"] == "hi"
+
+    def test_match__associate_name_nested(self):
+        tokenizer = DefaultTokenizer()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        result = matcher.match(
+            message="millet",
+            template="[[galangal|millet]~name|hi]~greeting",
+        )
+
+        assert result
+        assert result.success
+        assert result.context["greeting"] == "millet"
+        assert result.context["name"] == "millet"
+
+    def test_match__associate_name_nested_second(self):
+        tokenizer = DefaultTokenizer()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        result = matcher.match(
+            message="hi",
+            template="[[galangal|millet]~name|hi]~greeting",
+        )
+
+        assert result
+        assert result.success
+        assert result.context["greeting"] == "hi"
+        assert "name" not in result.context
+
+    def test_match__associate_name_few(self):
+        tokenizer = DefaultTokenizer()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        result = matcher.match(
+            message="millet hello",
+            template="[galangal|millet]~name [hello|hi]~greeting",
+        )
+
+        assert result
+        assert result.success
+        assert result.context["name"] == "millet"
+        assert result.context["greeting"] == "hello"
+
+    def test_match__associate_name_any(self):
+        tokenizer = DefaultTokenizer()
+        matcher = Matcher(tokenizer=tokenizer)
+
+        result = matcher.match(
+            message="millet hello",
+            template="*~name hello",
+        )
+
+        assert result
+        assert result.success
+        assert result.context["name"] == "millet"
 
 
-def test_incorrect_message():
-    tokens = [
-        CharToken(char="h"),
-        CharToken(char="e"),
-        AnyToken(),
-        CharToken(char="a"),
-    ]
-    match_tokens_with_message_results = Matcher(tokenizer=None).match_with_tokens(
-        tokens=tokens,
-        message="hello",
-    )
-    assert match_tokens_with_message_results == []
+class TestDefaultMatcher:
+    def test_match(self):
+        matcher = DefaultMatcher()
 
+        result = matcher.match(
+            message="hello my name is Docker",
+            template="[hello|hi] my name is *",
+        )
 
-def test_tokens_and_empty_message():
-    tokens = [
-        CharToken(char="h"),
-        CharToken(char="e"),
-        AnyToken(),
-        CharToken(char="o"),
-    ]
-    match_tokens_with_message_results = Matcher(tokenizer=None).match_with_tokens(
-        tokens=tokens,
-        message="",
-    )
-    assert match_tokens_with_message_results == []
+        assert result
+        assert result.success
+        assert result.context == {}
 
+    def test_match__incorrect(self):
+        matcher = DefaultMatcher()
 
-def test_empty_tokens_and_empty_message():
-    tokens = []
-    match_tokens_with_message_results = Matcher(tokenizer=None).match_with_tokens(
-        tokens=tokens,
-        message="",
-    )
-    assert match_tokens_with_message_results[0].end == 0
+        result = matcher.match(
+            message="hallo my name is Docker",
+            template="[hello|hi] my name is *",
+        )
 
-
-def test_empty_tokens_and_message():
-    tokens = []
-    match_tokens_with_message_results = Matcher(tokenizer=None).match_with_tokens(
-        tokens=tokens,
-        message="hello",
-    )
-    assert match_tokens_with_message_results[0].end == 0
-
-
-def test_any_variants():
-    tokens = [
-        AnyToken(),
-        CharToken(char="e"),
-    ]
-    match_tokens_with_message_results = Matcher(tokenizer=None).match_with_tokens(
-        tokens=tokens,
-        message="hee",
-    )
-    assert match_tokens_with_message_results[0].end == 2
-    assert match_tokens_with_message_results[1].end == 3
-
-
-def test_any_token():
-    tokens = [
-        AnyToken(),
-    ]
-    match_tokens_with_message_results = Matcher(tokenizer=None).match_with_tokens(
-        tokens=tokens,
-        message="hee",
-    )
-    assert match_tokens_with_message_results[0].end == 1
-    assert match_tokens_with_message_results[1].end == 2
-    assert match_tokens_with_message_results[2].end == 3
-
-
-def test_default_matcher_success():
-    matcher = DefaultMatcher()
-
-    result = matcher.match(
-        message="hello my name is Docker",
-        template="[hello|hi] my name is *",
-    )
-
-    assert result
-
-
-def test_associate_name():
-    matcher = DefaultMatcher()
-
-    result = matcher.match(
-        message="hello",
-        template="[hello|hi]~greeting",
-    )
-
-    assert result
-    assert result.context["greeting"] == "hello"
-
-
-def test_associate_name_2():
-    matcher = DefaultMatcher()
-
-    result = matcher.match(
-        message="hi",
-        template="[hello|hi]~greeting",
-    )
-
-    assert result
-    assert result.context["greeting"] == "hi"
-
-
-def test_associate_name_nested():
-    matcher = DefaultMatcher()
-
-    result = matcher.match(
-        message="millet",
-        template="[[galangal|millet]~name|hi]~greeting",
-    )
-
-    assert result
-    assert result.context["greeting"] == "millet"
-    assert result.context["name"] == "millet"
-
-
-def test_associate_name_nested_2():
-    matcher = DefaultMatcher()
-
-    result = matcher.match(
-        message="hi",
-        template="[[galangal|millet]~name|hi]~greeting",
-    )
-
-    assert result
-    assert result.context["greeting"] == "hi"
-    assert "name" not in result.context
-
-
-def test_associate_name_few():
-    matcher = DefaultMatcher()
-
-    result = matcher.match(
-        message="millet hello",
-        template="[galangal|millet]~name [hello|hi]~greeting",
-    )
-
-    assert result
-    assert result.context["name"] == "millet"
-    assert result.context["greeting"] == "hello"
-
-
-def test_associate_name_any():
-    matcher = DefaultMatcher()
-
-    result = matcher.match(
-        message="millet hello",
-        template="*~name hello",
-    )
-
-    assert result
-    assert result.context["name"] == "millet"
-
-
-def test_default_matcher_fail():
-    matcher = DefaultMatcher()
-
-    result = matcher.match(
-        message="hallo my name is Docker",
-        template="[hello|hi] my name is *",
-    )
-
-    assert not result
+        assert not result
+        assert not result.success
+        assert result.context == {}
